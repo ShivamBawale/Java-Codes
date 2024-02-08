f43e13782050eade0d1d34432fd39dacb376da22
ShivamBawale <126599801+ShivamBawale@users.noreply.github.com>
2024-02-08 11:17:26 +0530

    Add files via upload

---
+++ Cylindercons.java
@@ -0,0 +1,35 @@
+
+package cylindercons;
+
+class cylinder{
+    private double radius;
+    private double height;
+    
+    public double getRadius()
+    {
+        return radius;
+    }
+    public double getHeight()
+    {
+        return height;
+    }
+    public double setRadius(double r)
+    {
+        radius=r;
+    }
+    public double setHeight(double h)
+    {
+        height=r;
+    }
+    
+}
+
+
+public class Cylindercons {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
