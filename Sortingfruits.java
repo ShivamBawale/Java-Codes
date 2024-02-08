7ba1822d9d8a3a3d6d42383a107c8b2cb2721850
ShivamBawale <126599801+ShivamBawale@users.noreply.github.com>
2024-02-08 11:34:24 +0530

    Add files via upload

---
+++ Sortingfruits.java
@@ -0,0 +1,23 @@
+
+package sortingfruits;
+
+import java.util.Arrays;
+
+
+public class Sortingfruits {
+
+
+    public static void main(String[] args) {
+        
+        String[] names={"shivam","yasin","abhishek"};
+        Arrays.sort(names);
+        
+        for(String name:names)
+        {
+            System.out.println(name+"");
+        }
+        
+        
+    }
+    
+}
