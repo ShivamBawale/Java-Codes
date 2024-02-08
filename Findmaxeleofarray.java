9137a930a52c254f5350c87c82487d00f7f9b1f9
ShivamBawale <126599801+ShivamBawale@users.noreply.github.com>
2024-02-08 11:21:52 +0530

    Add files via upload

---
+++ Findmaxeleofarray.java
@@ -0,0 +1,17 @@
+
+package findmaxeleofarray;
+
+
+public class Findmaxeleofarray {
+    Ststic int max(A[])
+    {
+        for(int i=0;i<A.)
+    }
+   
+    public static void main(String[] args) {
+        int A[]={1,2,3,4,5,6};
+        
+        
+    }
+    
+}
